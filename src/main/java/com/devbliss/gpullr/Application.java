c1c2fec163109121606847d58fd8645718927763
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-02-04 11:11:00 +0100

    Application entry point

---
+++ src/main/java/com/devbliss/gpullr/Application.java
@@ -0,0 +1,60 @@
+package com.devbliss.gpullr;
+
+import javax.sql.DataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+/**
+ * @author Daniel Walldorf <daniel.walldorf@devbliss.com>
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  @Profile("test")
+  public DataSource createTestDataSource() {
+    return DataSourceBuilder.create().url("jdbc:h2:mem:task_test").driverClassName("org.h2.Driver").build();
+  }
+
+  @Bean
+  @Profile("prod")
+  public DataSource createDataSource() {
+    return DataSourceBuilder.create().url("jdbc:h2:./task").driverClassName("org.h2.Driver").build();
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
+    JpaVendorAdapter jpaVendorAdapter) {
+    LocalContainerEntityManagerFactoryBean emfb = new LocalContainerEntityManagerFactoryBean();
+    emfb.setDataSource(dataSource);
+    emfb.setPackagesToScan("gtd.domain");
+    emfb.setJpaVendorAdapter(jpaVendorAdapter);
+    return emfb;
+  }
+
+  @Bean
+  public JpaVendorAdapter createJpaVendorAdapter() {
+    HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+    adapter.setDatabase(Database.H2);
+    adapter.setShowSql(true);
+    adapter.setGenerateDdl(true);
+    adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
+    return adapter;
+  }
+
+}
