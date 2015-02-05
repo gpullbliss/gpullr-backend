395dd38f1a555ad2e52532dbaa83e7b3e2ade6ff
hschuetz <henning.schuetz@devbliss.com>
2015-02-05 11:00:57 +0100

    set server port to 8888; logback conf and logger injector

---
+++ src/main/java/com/devbliss/gpullr/util/Log.java
@@ -0,0 +1,19 @@
+package com.devbliss.gpullr.util;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to inject {@link org.slf4j.Logger} with the class name.
+ *
+ */
+@Retention(RUNTIME)
+@Target(FIELD)
+@Documented
+public @interface Log {
+
+}
