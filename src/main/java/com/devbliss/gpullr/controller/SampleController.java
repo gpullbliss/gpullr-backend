2d7c08da519277d95545acea1d97956810530b71
hschuetz <henning.schuetz@devbliss.com>
2015-02-09 16:08:45 +0100

    checkstyle

--- src/main/java/com/devbliss/gpullr/controller/SampleController.java
+++ src/main/java/com/devbliss/gpullr/controller/SampleController.java
@@ -2,12 +2,11 @@ package com.devbliss.gpullr.controller;
 
 
 import com.devbliss.gpullr.service.github.GithubService;
-
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
