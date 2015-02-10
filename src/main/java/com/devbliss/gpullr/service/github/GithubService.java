9ca716cd8a30028e0788f44bf90a05fe8ac72529
hschuetz <henning.schuetz@devbliss.com>
2015-02-10 13:57:21 +0100

    work in progress

--- src/main/java/com/devbliss/gpullr/service/github/GithubService.java
+++ src/main/java/com/devbliss/gpullr/service/github/GithubService.java
@@ -1,9 +1,9 @@
 package com.devbliss.gpullr.service.github;
 
-import com.jcabi.github.Github;
 import com.jcabi.http.response.JsonResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.json.JsonObject;
 import javax.json.JsonValue.ValueType;
@@ -28,16 +28,34 @@ public class GithubService {
    * @throws IOException
    */
   public List<String> proofThatItWorks() throws IOException {
-    Github client = githubFactory.createClient();
+    return loadAllPages("/orgs/devbliss/repos", jo -> jo.getString("name") + " _ " + jo.getString("html_url"));
+  }
+  
+  public void loadEvents() throws IOException {
+     // /repos/:owner/:repo/events
+  }
+  
+  private <T> List<T> loadAllPages(String path, Function<JsonObject, T> mapper) throws IOException {
+    final JsonResponse resp = githubFactory.createClient().entry().uri().path(path).back().fetch().as(JsonResponse.class);
+    return handleResponse(resp, mapper, path, 1);
+  }
 
-    final JsonResponse resp = client.entry().uri().path("/orgs/devbliss/repos").back().fetch().as(JsonResponse.class);
-    return resp
-      .json()
-      .readArray()
-      .stream()
-      .filter(v -> v.getValueType() == ValueType.OBJECT)
-      .map(v -> (JsonObject) v)
-      .map(o -> o.getString("name") + ": " + o.getString("html_url"))
-      .collect(Collectors.toList());
+  private <T> List<T> handleResponse(JsonResponse resp, Function<JsonObject, T> mapper, String path, int page)  throws IOException {
+    System.err.println("handle response page: " + page);
+    List<T> result = resp
+        .json()
+        .readArray()
+        .stream()
+        .filter(v -> v.getValueType() == ValueType.OBJECT)
+        .map(v -> (JsonObject) v)
+        .map(mapper)
+        .collect(Collectors.toList());    
+    
+    if (resp.headers().keySet().contains("Link") && resp.headers().get("Link").stream().anyMatch(s -> s.contains("next"))) {
+      resp = githubFactory.createClient().entry().uri().path(path).queryParam("page", page) .back().fetch().as(JsonResponse.class);
+      result.addAll(handleResponse(resp, mapper, path, page + 1));
+    }
+    
+    return result;
   }
 }
