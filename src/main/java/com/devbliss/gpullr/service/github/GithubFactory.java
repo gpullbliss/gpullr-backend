5cd4a6990ba76467c1960fd86d0b9de8e6065ada
hschuetz <henning.schuetz@devbliss.com>
2015-02-10 14:06:08 +0100

    cleaned up github client creation

--- src/main/java/com/devbliss/gpullr/service/github/GithubFactory.java
+++ src/main/java/com/devbliss/gpullr/service/github/GithubFactory.java
@@ -1,8 +1,35 @@
 package com.devbliss.gpullr.service.github;
 
-import com.jcabi.github.Github;
+import com.jcabi.github.mock.MkGithub;
+import java.io.IOException;
 
-public interface GithubFactory {
+import com.jcabi.github.Github;
+import com.jcabi.github.RtGithub;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GithubFactory {
+
+  @Value("${github.oauthtoken}")
+  private String oauthToken;
+
+  @Bean
+  @Profile({"prod", "dev"})
+  public Github createClient() {
+    return new RtGithub(oauthToken);
+  }
   
-  Github createClient();
+  @Bean
+  @Profile("test")
+  public Github createTestClient() {
+    try {
+      return new MkGithub();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 }
--- src/main/java/com/devbliss/gpullr/service/github/GithubService.java
+++ src/main/java/com/devbliss/gpullr/service/github/GithubService.java
@@ -1,5 +1,6 @@
 package com.devbliss.gpullr.service.github;
 
+import com.jcabi.github.Github;
 import com.jcabi.http.response.JsonResponse;
 import java.io.IOException;
 import java.util.List;
@@ -20,7 +21,9 @@ import org.springframework.stereotype.Service;
 public class GithubService {
 
   @Autowired
-  private GithubFactory githubFactory;
+  private Github client;
+
+  // private GithubFactory githubFactory;
 
   /**
    * Reads all repositories owned bei devbliss and returns a list of strings in the format name:url
@@ -30,32 +33,34 @@ public class GithubService {
   public List<String> proofThatItWorks() throws IOException {
     return loadAllPages("/orgs/devbliss/repos", jo -> jo.getString("name") + " _ " + jo.getString("html_url"));
   }
-  
+
   public void loadEvents() throws IOException {
-     // /repos/:owner/:repo/events
+    // /repos/:owner/:repo/events
   }
-  
+
   private <T> List<T> loadAllPages(String path, Function<JsonObject, T> mapper) throws IOException {
-    final JsonResponse resp = githubFactory.createClient().entry().uri().path(path).back().fetch().as(JsonResponse.class);
+    final JsonResponse resp = client.entry().uri().path(path).back().fetch().as(JsonResponse.class);
     return handleResponse(resp, mapper, path, 1);
   }
 
-  private <T> List<T> handleResponse(JsonResponse resp, Function<JsonObject, T> mapper, String path, int page)  throws IOException {
+  private <T> List<T> handleResponse(JsonResponse resp, Function<JsonObject, T> mapper, String path, int page)
+      throws IOException {
     System.err.println("handle response page: " + page);
     List<T> result = resp
-        .json()
-        .readArray()
-        .stream()
-        .filter(v -> v.getValueType() == ValueType.OBJECT)
-        .map(v -> (JsonObject) v)
-        .map(mapper)
-        .collect(Collectors.toList());    
-    
-    if (resp.headers().keySet().contains("Link") && resp.headers().get("Link").stream().anyMatch(s -> s.contains("next"))) {
-      resp = githubFactory.createClient().entry().uri().path(path).queryParam("page", page) .back().fetch().as(JsonResponse.class);
+      .json()
+      .readArray()
+      .stream()
+      .filter(v -> v.getValueType() == ValueType.OBJECT)
+      .map(v -> (JsonObject) v)
+      .map(mapper)
+      .collect(Collectors.toList());
+
+    if (resp.headers().keySet().contains("Link")
+        && resp.headers().get("Link").stream().anyMatch(s -> s.contains("next"))) {
+      resp = client.entry().uri().path(path).queryParam("page", page).back().fetch().as(JsonResponse.class);
       result.addAll(handleResponse(resp, mapper, path, page + 1));
     }
-    
+
     return result;
   }
 }
