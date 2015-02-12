bc1be5948035c53899899277ebc6aab80d063c76
tumminelli <antonino.tumminelli@devbliss.com>
2015-02-12 14:28:17 +0100

    renamed PullrequestController

--- src/main/java/com/devbliss/gpullr/controller/PullrequestController.java
+++ src/main/java/com/devbliss/gpullr/controller/PullrequestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/pulls")
-public class PullrequestsController {
+public class PullrequestController {
 
   @RequestMapping(method = RequestMethod.GET)
   public List<Pullrequest> getPullrequests() {
