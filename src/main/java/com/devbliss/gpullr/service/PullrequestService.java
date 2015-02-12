422882477ae07c9b04a44d6558b337372b2659b1
hschuetz <henning.schuetz@devbliss.com>
2015-02-12 14:23:39 +0100

    pullrequest repo and service

---
+++ src/main/java/com/devbliss/gpullr/service/PullrequestService.java
@@ -0,0 +1,21 @@
+package com.devbliss.gpullr.service;
+
+import com.devbliss.gpullr.domain.Pullrequest;
+import com.devbliss.gpullr.repository.PullrequestRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PullrequestService {
+
+  private final PullrequestRepository pullrequestRepository;
+  
+  @Autowired
+  public PullrequestService(PullrequestRepository pullrequestRepository) {
+    this.pullrequestRepository = pullrequestRepository;
+  }
+  
+  public List<Pullrequest> findAll() {
+    // TODO sort
+    return pullrequestRepository.findAll();
+  }
+}
