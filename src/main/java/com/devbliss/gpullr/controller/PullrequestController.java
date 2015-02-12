7630b6d05c9fca7daa72ca6c207fe2a3f21e1f0c
tumminelli <antonino.tumminelli@devbliss.com>
2015-02-12 15:12:08 +0100

    some changes

--- src/main/java/com/devbliss/gpullr/controller/PullrequestController.java
+++ src/main/java/com/devbliss/gpullr/controller/PullrequestController.java
@@ -1,8 +1,11 @@
 package com.devbliss.gpullr.controller;
 
 import com.devbliss.gpullr.domain.Pullrequest;
+import com.devbliss.gpullr.service.PullrequestService;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,15 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pulls")
 public class PullrequestController {
 
+  @Autowired
+  PullrequestService pullrequestService;
+
   @RequestMapping(method = RequestMethod.GET)
   public List<Pullrequest> getPullrequests() {
     List<Pullrequest> pullrequests = new ArrayList<Pullrequest>();
+    pullrequests = pullrequestService.findAll();
+
     return pullrequests;
   }
 
-  @RequestMapping(method = RequestMethod.POST)
-  public void assignPullrequest(@RequestBody String GitHubOAuthToken) {
-
+  @RequestMapping(method = RequestMethod.POST, value = "/{pullrequestId}")
+  public void assignPullrequest(@RequestBody String sessionId, @PathVariable Integer pullrequestId) {
+    pullrequestService.assignPullrequest(sessionId, pullrequestId);
   }
 
 }
--- src/main/java/com/devbliss/gpullr/service/PullrequestService.java
+++ src/main/java/com/devbliss/gpullr/service/PullrequestService.java
@@ -8,14 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class PullrequestService {
 
   private final PullrequestRepository pullrequestRepository;
-  
+
   @Autowired
   public PullrequestService(PullrequestRepository pullrequestRepository) {
     this.pullrequestRepository = pullrequestRepository;
   }
-  
+
   public List<Pullrequest> findAll() {
     // TODO sort
     return pullrequestRepository.findAll();
+  }
+
+  public void assignPullrequest(String sessionId, Integer pullrequestId) {
+
   }
 }
