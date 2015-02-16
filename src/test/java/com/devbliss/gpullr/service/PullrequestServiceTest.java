58395067c945ed9db3e373d491776b642fb6ef71
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-02-16 16:29:44 +0100

    added missed class documentation

--- src/test/java/com/devbliss/gpullr/service/PullrequestServiceTest.java
+++ src/test/java/com/devbliss/gpullr/service/PullrequestServiceTest.java
@@ -20,6 +20,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+/**
+ * Tests for {@link PullrequestService}
+ *
+ * @author Philipp Karstedt <philipp.karstedt@devbliss.com>
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
