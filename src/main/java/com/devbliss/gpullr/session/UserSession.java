3b25c2637f8621cfde390c7ec81e95df582bf4b7
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-02-18 16:27:05 +0100

    WIP UserSession

--- src/main/java/com/devbliss/gpullr/session/UserSession.java
+++ src/main/java/com/devbliss/gpullr/session/UserSession.java
@@ -1,6 +1,9 @@
 package com.devbliss.gpullr.session;
 
 import com.devbliss.gpullr.domain.User;
+import com.devbliss.gpullr.util.Log;
+import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
@@ -14,5 +17,14 @@ import org.springframework.web.context.WebApplicationContext;
 @Component
 @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UserSession {
+
+  @Log
+  Logger logger;
+
   public User user;
+
+  @PostConstruct
+  public void userSession() {
+    logger.debug("############################### Fucking PostConstruct of userSession ###################################");
+  }
 }
