43768c47dc49d7b4ed76b65f0e9c807a9d859153
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-02-18 16:21:49 +0100

    WIP UserController

--- src/main/java/com/devbliss/gpullr/controller/UserController.java
+++ src/main/java/com/devbliss/gpullr/controller/UserController.java
@@ -3,11 +3,16 @@ package com.devbliss.gpullr.controller;
 import com.devbliss.gpullr.domain.User;
 import com.devbliss.gpullr.service.UserService;
 import com.devbliss.gpullr.session.UserSession;
+import com.devbliss.gpullr.util.Log;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,6 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Log
+  private Logger logger;
+
   @Autowired
   private UserService userService;
 
@@ -26,16 +34,26 @@ public class UserController {
   private UserSession userSession;
 
   @RequestMapping(method = RequestMethod.GET)
-  public List<User> getAllUsers() {
-    return userService.findAll();
+  public List<User> getAllOrgaMembers() {
+    return userService.findAllOrgaMembers();
   }
 
   @RequestMapping(value = "/login/{id}", method = RequestMethod.POST)
-  public User getNothing(@PathVariable("id") int id) {
-    System.out.println("########### Da sindwa drin! " + id);
+  @ResponseStatus(HttpStatus.CREATED)
+  public void login(@PathVariable("id") int id, HttpServletRequest request) {
     // find user in db and set for userSession
     User loggedInUser = userService.findById(id);
     userSession.user = loggedInUser;
-    return loggedInUser;
+    logger.debug("Login for: " + loggedInUser.username + " with session: " + request.getSession());
   }
+
+  @RequestMapping(value = "/me", method = RequestMethod.GET)
+  public User whoAmI() {
+    if (userSession.user != null) {
+      return userSession.user;
+    } else {
+      return null;
+    }
+  }
+
 }
