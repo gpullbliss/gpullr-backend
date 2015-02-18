c5715155f6d37411306ef5d5898297d514da610c
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-02-18 10:24:47 +0100

    WIP user session

---
+++ src/main/java/com/devbliss/gpullr/session/UserSession.java
@@ -0,0 +1,19 @@
+package com.devbliss.gpullr.session;
+
+import com.devbliss.gpullr.domain.User;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * Represent the session of a user and includes its {@link User} object.
+ * 
+ * @author Philipp Karstedt <philipp.karstedt@devbliss.com>
+ *
+ */
+@Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class UserSession {
+  public User user;
+}
