44d0f47d15865b7df999b60e71da197233bfe5f2
Alexander Bluem <alexander.bluem@devbliss.com>
2015-02-19 13:25:05 +0100

    remove stacktrace

--- src/main/java/com/devbliss/gpullr/controller/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/devbliss/gpullr/controller/GlobalDefaultExceptionHandler.java
@@ -15,27 +15,11 @@ public class GlobalDefaultExceptionHandler {
 
   @ExceptionHandler(value = LoginRequiredException.class)
   public ResponseEntity<String> defaultForbidden(HttpServletRequest request, Exception e) throws Exception {
-    String commentStackTrace = createCommentStackTrace(
-        String.format("'%s' requires log in", request.getRequestURL().toString()), e);
-
     StringBuilder responseBody = new StringBuilder();
     responseBody.append("<h1>FORBIDDEN</h1>\n");
-    responseBody.append(commentStackTrace);
+    responseBody.append(String.format("log in required for '%s'", request.getRequestURL().toString()));
 
     return new ResponseEntity<String>(responseBody.toString(), HttpStatus.FORBIDDEN);
-  }
-
-  private String createCommentStackTrace(String reason, Exception e) {
-    StringBuilder buf = new StringBuilder();
-    buf.append("<!--\n");
-    buf.append(reason);
-    buf.append('\n');
-
-    for (StackTraceElement s : e.getStackTrace()) {
-      buf.append(s.toString() + "\n");
-    }
-    buf.append("-->");
-    return buf.toString();
   }
 
 }
