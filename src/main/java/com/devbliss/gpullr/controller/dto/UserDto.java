1a6d00d4ecfdcfc49088b32fd0085d4987d67cda
Alexander Bluem <alexander.bluem@devbliss.com>
2015-02-19 14:00:04 +0100

    satisfy checkstyle

--- src/main/java/com/devbliss/gpullr/controller/dto/UserDto.java
+++ src/main/java/com/devbliss/gpullr/controller/dto/UserDto.java
@@ -1,13 +1,12 @@
 package com.devbliss.gpullr.controller.dto;
 
-import com.devbliss.gpullr.domain.User;
-
 /**
- * DTO for JSON de-/serialization of {@link User} instances.
- *
+ * DTO for JSON de-/serialization of {@link com.devbliss.gpullr.domain.User} instances.
  */
 public class UserDto {
   public int id;
+
   public String username;
+
   public String avatarUrl;
 }
