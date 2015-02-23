3e8546801052e40cc9bca35e98a08011999903e0
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-02-23 12:58:11 +0100

    user ListDto for /pulls endpoint

--- src/main/java/com/devbliss/gpullr/controller/PullrequestController.java
+++ src/main/java/com/devbliss/gpullr/controller/PullrequestController.java
@@ -1,19 +1,18 @@
 package com.devbliss.gpullr.controller;
 
-import org.springframework.http.HttpStatus;
-
-import org.springframework.web.bind.annotation.ResponseStatus;
-import javax.validation.constraints.NotNull;
+import com.devbliss.gpullr.controller.dto.ListDto;
 import com.devbliss.gpullr.controller.dto.PullrequestConverter;
 import com.devbliss.gpullr.controller.dto.PullrequestDto;
 import com.devbliss.gpullr.service.PullrequestService;
 import com.devbliss.gpullr.service.UserService;
-import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -33,12 +32,12 @@ public class PullrequestController {
   private UserService userService;
 
   @RequestMapping(method = RequestMethod.GET)
-  public List<PullrequestDto> findAll() {
-    return pullrequestService
+  public ListDto<PullrequestDto> findAll() {
+    return new ListDto<PullrequestDto>(pullrequestService
       .findAllOpen()
       .stream()
       .map(pr -> pullrequestConverter.toDto(pr))
-      .collect(Collectors.toList());
+      .collect(Collectors.toList()));
   }
 
   @RequestMapping(method = RequestMethod.POST, value = "/{pullrequestId}")
