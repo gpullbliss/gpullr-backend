55d7c20f982f0fe74095b53ecda3c018da9cc45f
Alexander Bluem <alexander.bluem@devbliss.com>
2015-02-26 11:42:07 +0100

    uppercase key value

--- src/main/java/com/devbliss/gpullr/util/Constants.java
+++ src/main/java/com/devbliss/gpullr/util/Constants.java
@@ -6,7 +6,7 @@ package com.devbliss.gpullr.util;
 
 public class Constants {
 
-  public static String KEY_DTO_ERROR_FORBIDDEN = "forbidden";
+  public static String KEY_DTO_ERROR_FORBIDDEN = "FORBIDDEN";
 
 }
 
