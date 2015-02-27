02209b73232a72f73a3f9dcda685d85b3d224061
hschuetz <henning.schuetz@devbliss.com>
2015-02-27 17:11:26 +0100

    log debug

--- src/main/java/com/devbliss/gpullr/service/github/PullRequestEventHandler.java
+++ src/main/java/com/devbliss/gpullr/service/github/PullRequestEventHandler.java
@@ -49,7 +49,7 @@ public class PullRequestEventHandler {
     logger.debug("handling pr ev: " + pullRequestFromEvent.title + " / " + pullRequestFromEvent.state);
 
     if (pullRequestFromEvent.assignee != null) {
-      logger.debug("2assigned_assignee_to_pullrequest: " + pullRequestFromEvent.title + " // "
+      logger.debug("3assigned_assignee_to_pullrequest: " + pullRequestFromEvent.title + " // "
           + pullRequestFromEvent.assignee + " // " + pullRequestFromEvent.state);
     }
     pullRequestService.insertOrUpdate(pullRequestFromEvent);
