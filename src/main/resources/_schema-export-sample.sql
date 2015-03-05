0a311045c2c584cfff14e78a472ed691accff074
hschuetz <henning.schuetz@devbliss.com>
2015-03-05 17:58:58 +0100

    complete schema export

---
+++ src/main/resources/_schema-export-sample.sql
@@ -0,0 +1,4 @@
+-- This SQL command can be used to create an SQL schema file e.g. from a JPA auto-generated H2 database. Can be fired e.g. from H2 web console.
+-- See http://h2database.com/html/grammar.html#script for more information.
+
+SCRIPT NODATA NOPASSWORDS NOSETTINGS TO '/path/to/your/project/schema-export-h2.sql' CHARSET 'UTF-8';
