f15f725f0718baf8669ce1dc3c40a1a5f131170e
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-03-10 09:42:13 +0100

    Implemented UserSettings domain object

---
+++ src/main/java/com/devbliss/gpullr/domain/UserSettings.java
@@ -0,0 +1,36 @@
+package com.devbliss.gpullr.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Individual settings for a user of the application.
+ */
+@Entity
+public class UserSettings {
+
+  public enum OrderOption {
+    ASC,
+    DESC
+  }
+
+  @Id
+  @Column(name = "user_settings_id")
+  @GeneratedValue
+  public Integer id;
+
+  @Column
+  @Enumerated(value = EnumType.STRING)
+  public OrderOption defaultPullRequestListOrdering;
+
+  public UserSettings() {
+  }
+
+  public UserSettings(OrderOption ordering) {
+    this.defaultPullRequestListOrdering = ordering;
+  }
+}
