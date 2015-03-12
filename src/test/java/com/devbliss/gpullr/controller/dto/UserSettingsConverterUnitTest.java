5a8560680c1f3578e0595e60073f1d8023abc1bb
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-03-12 13:36:21 +0100

    Unit tests for UserSettingsConverter

---
+++ src/test/java/com/devbliss/gpullr/controller/dto/UserSettingsConverterUnitTest.java
@@ -0,0 +1,51 @@
+package com.devbliss.gpullr.controller.dto;
+
+import static org.junit.Assert.*;
+
+import com.devbliss.gpullr.domain.UserSettings;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * Unit tests for {@link com.devbliss.gpullr.controller.dto.UserSettingsConverter}.
+ */
+public class UserSettingsConverterUnitTest {
+
+  @InjectMocks
+  private UserSettingsConverter userSettingsConverter;
+
+  private UserSettings userSettings;
+
+  private UserSettingsDto userSettingsDto;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    userSettings = new UserSettings();
+    userSettings.id = 1;
+    userSettings.defaultPullRequestListOrdering = UserSettings.OrderOption.ASC;
+
+    userSettingsDto = new UserSettingsDto();
+    userSettingsDto.id = 2;
+    userSettingsDto.orderOptionDto = UserSettingsDto.OrderOptionDto.DESC;
+  }
+
+  @Test
+  public void toDto() {
+    UserSettingsDto dto = userSettingsConverter.toDto(userSettings);
+
+    assertEquals(userSettings.id, dto.id);
+    assertEquals(userSettings.defaultPullRequestListOrdering.name(), dto.orderOptionDto.name());
+  }
+
+  @Test
+  public void toEntity() {
+    UserSettings entity = userSettingsConverter.toEntity(userSettingsDto);
+
+    assertEquals(userSettingsDto.id, entity.id);
+    assertEquals(userSettingsDto.orderOptionDto.name(), entity.defaultPullRequestListOrdering.name());
+  }
+
+}
