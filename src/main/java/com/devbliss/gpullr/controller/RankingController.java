2f08792fb7c57307af2caf7921927f70303c97bd
hschuetz <henning.schuetz@devbliss.com>
2015-03-18 13:38:20 +0100

    unused import

--- src/main/java/com/devbliss/gpullr/controller/RankingController.java
+++ src/main/java/com/devbliss/gpullr/controller/RankingController.java
@@ -10,11 +10,9 @@ import com.devbliss.gpullr.domain.RankingScope;
 import com.devbliss.gpullr.service.RankingService;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
