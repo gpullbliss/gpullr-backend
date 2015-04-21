f910e19821c9d3117aa410a12342b006f2551370
hschuetz <henning.schuetz@devbliss.com>
2015-04-21 15:40:01 +0200

    websockets work in progress

--- src/main/java/com/devbliss/gpullr/controller/NotificationWebsocketController.java
+++ src/main/java/com/devbliss/gpullr/controller/NotificationWebsocketController.java
@@ -5,9 +5,12 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class NotificationWebsocketController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(NotificationWebsocketController.class);
@@ -17,19 +20,12 @@ public class NotificationWebsocketController {
     LOGGER.debug("NotificationWebsocken-Kontrolleur is am Start...");
   }
 
-  
-//  @SendTo("/notifications/live/queue")
-//  public NotificationDto bla(String message) {
-//    LOGGER.debug("WS received: " + message);
-//    NotificationDto notificationDto = new NotificationDto();
-//    notificationDto.actorName = "Blubberbla: " + message;
-//    return notificationDto;
-//  }
-  
-  @MessageMapping("/einfach")
-  public void bla(String message) {
-    LOGGER.debug("WS received: " + message);
+  @MessageMapping("/notify")
+  @SendTo("/topic/message")
+  public NotificationDto bla(NotificationDto input) {
+    LOGGER.debug("WS received: " + input);
     NotificationDto notificationDto = new NotificationDto();
-    notificationDto.actorName = "Blubberbla: " + message;
+    notificationDto.actorName = "Blubberbla: " + input.repoTitle;
+    return notificationDto;
   }
 }
--- src/main/java/com/devbliss/gpullr/controller/WebsocketConfig.java
+++ src/main/java/com/devbliss/gpullr/controller/WebsocketConfig.java
@@ -1,6 +1,7 @@
 package com.devbliss.gpullr.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -10,8 +11,14 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 public class WebsocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
   @Override
+  public void configureMessageBroker(MessageBrokerRegistry config) {
+    config.enableSimpleBroker("/topic");
+    config.setApplicationDestinationPrefixes("/app");
+  }
+
+  @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/einfach");
+    registry.addEndpoint("/notifications").withSockJS();
   }
 
   // @Override
