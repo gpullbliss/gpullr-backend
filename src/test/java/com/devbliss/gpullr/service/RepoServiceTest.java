57b1eae87cd17f0802f5c733c149e9a30d6a9bf9
Daniel Walldorf <entekmedia@gmail.com>
2015-04-22 07:51:56 +0200

    checkstyle fix

--- src/test/java/com/devbliss/gpullr/service/RepoServiceTest.java
+++ src/test/java/com/devbliss/gpullr/service/RepoServiceTest.java
@@ -1,6 +1,8 @@
 package com.devbliss.gpullr.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
