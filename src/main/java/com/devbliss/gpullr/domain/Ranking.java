71dbf8b68084a62d7d1faa495d495a323e67ac3e
hschuetz <henning.schuetz@devbliss.com>
2015-05-06 10:19:06 +0200

    checkstyle

--- src/main/java/com/devbliss/gpullr/domain/Ranking.java
+++ src/main/java/com/devbliss/gpullr/domain/Ranking.java
@@ -1,13 +1,12 @@
 package com.devbliss.gpullr.domain;
 
-import javax.persistence.Table;
-
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
