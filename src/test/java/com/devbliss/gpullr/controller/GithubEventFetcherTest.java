5fb850472adbf291a3a224e3b5c7569749168f7c
hschuetz <henning.schuetz@devbliss.com>
2015-05-06 17:54:41 +0200

    eob

--- src/test/java/com/devbliss/gpullr/controller/GithubEventFetcherTest.java
+++ src/test/java/com/devbliss/gpullr/controller/GithubEventFetcherTest.java
@@ -5,7 +5,9 @@ import static org.mockito.Mockito.verify;
 import com.devbliss.gpullr.Application;
 import com.devbliss.gpullr.domain.Repo;
 import com.devbliss.gpullr.domain.RepoCreatedEvent;
+import com.devbliss.gpullr.service.RepoService;
 import com.devbliss.gpullr.service.github.GithubApi;
+import java.util.Arrays;
 import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +15,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -23,6 +26,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @ActiveProfiles("test")
 public class GithubEventFetcherTest {
 
+  private static final String ORIGINAL_REPO_NAME = "sometestrepo";
+
   @Autowired
   private GithubEventFetcher githubEventFetcher;
 
@@ -32,17 +37,29 @@ public class GithubEventFetcherTest {
   @Autowired
   private GithubApi githubApi;
 
+  @Autowired
+  private RepoService repoService;
+
+  @Autowired
+  private TaskScheduler taskScheduler;
+
   private Repo repo;
 
   @Before
   public void setup() {
-    repo = new Repo();
-    repo.name = "sometestrepo";
+    repo = new Repo(1, ORIGINAL_REPO_NAME, "");
+    repoService.setActiveRepos(Arrays.asList(repo));
   }
 
   @Test
   public void githubEventFetcherListensToRepoCreatedEvent() {
     applicationContext.publishEvent(new RepoCreatedEvent(this, repo));
     verify(githubApi).fetchAllEvents(repo, Optional.empty());
+  }
+
+  @Test
+  public void fetchLoop() {
+    githubEventFetcher.startFetchEventsLoop();
+    // verify(taskScheduler).schedule(task, startTime) ==> continue
   }
 }
--- src/test/java/com/devbliss/gpullr/util/TaskSchedulerMockProducer.java
+++ src/test/java/com/devbliss/gpullr/util/TaskSchedulerMockProducer.java
@@ -1,5 +1,8 @@
 package com.devbliss.gpullr.util;
 
+import org.mockito.Answers;
+
+import static org.mockito.Mockito.mock;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,25 +12,18 @@ import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
- * Produces instances of {@link TaskScheduler} which are already initialized and ready for use.
- * Active only for prod and dev profile. For tests, there is {@link TaskSchedulerMockProducer}.
- * 
+ * Produces Mockito mocks of {@link TaskScheduler} that can be used in verify calls etc..
  * 
  * @author Henning Schütz <henning.schuetz@devbliss.com>
  *
  */
 @Configuration
-public class TaskSchedulerProducer {
+public class TaskSchedulerMockProducer {
 
   @Bean
   @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
-  @Profile({
-      "prod", "dev"
-  })
+  @Profile("test")
   public TaskScheduler createThreadPoolTaskScheduler() {
-    ThreadPoolTaskScheduler executor = new ThreadPoolTaskScheduler();
-    executor.setPoolSize(5);
-    executor.initialize();
-    return executor;
+    return mock(ThreadPoolTaskScheduler.class, Answers.RETURNS_DEEP_STUBS.get());
   }
 }
