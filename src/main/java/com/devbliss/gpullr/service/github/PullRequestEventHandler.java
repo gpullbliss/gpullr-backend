d39db25f3e87215c37cd48250a54261a0ce485c8
Andre Dörnbrack <andre.doernbrack@devbliss.com>
2015-05-21 09:25:28 +0200

    have fun with checkstyle

--- src/main/java/com/devbliss/gpullr/service/github/PullRequestEventHandler.java
+++ src/main/java/com/devbliss/gpullr/service/github/PullRequestEventHandler.java
@@ -5,12 +5,10 @@ import com.devbliss.gpullr.domain.PullRequest.State;
 import com.devbliss.gpullr.domain.PullRequestEvent;
 import com.devbliss.gpullr.service.PullRequestService;
 import com.devbliss.gpullr.util.Log;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 /**
  * Handles pullRequest events fetched from GitHub and triggers the appropriate action in business layer.
@@ -22,9 +20,6 @@ public class PullRequestEventHandler {
 
   @Log
   Logger logger;
-
-  @Autowired
-  private TaskScheduler taskScheduler;
 
   private final PullRequestService pullRequestService;
 
