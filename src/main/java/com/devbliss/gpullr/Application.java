955688c2216bc1555dbab1e98502538729bb4ed3
Alexander Blüm <alexander.bluem@devbliss.com>
2015-05-26 15:08:13 +0200

    remove repeated demonstration purpose api rate limit reached event throwing

--- src/main/java/com/devbliss/gpullr/Application.java
+++ src/main/java/com/devbliss/gpullr/Application.java
@@ -1,17 +1,9 @@
 package com.devbliss.gpullr;
 
-import com.devbliss.gpullr.domain.ApiRateLimitReachedEvent;
-import com.devbliss.gpullr.util.Log;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
 
 /**
  * Application entry point
@@ -21,20 +13,8 @@ import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
 @EnableScheduling
 public class Application {
 
-  @Log
-  private static Logger logger;
-
   public static void main(String[] args) {
-    ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-
-
-    TaskScheduler taskScheduler = new DefaultManagedTaskScheduler();
-
-    Runnable runnable = () -> {
-      ApiRateLimitReachedEvent event = new ApiRateLimitReachedEvent(run, Instant.now().plus(3, ChronoUnit.MINUTES));
-      run.publishEvent(event);
-    };
-
-    taskScheduler.scheduleAtFixedRate(runnable, 2 * 60 * 1000);
+    SpringApplication.run(Application.class, args);
   }
+
 }
