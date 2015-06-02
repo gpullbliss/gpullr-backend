b6db12f9d1994d465f369ac313732e834096ad64
hschuetz <henning.schuetz@devbliss.com>
2015-06-02 18:41:15 +0200

    javadoc

--- src/main/java/com/devbliss/gpullr/domain/Commit.java
+++ src/main/java/com/devbliss/gpullr/domain/Commit.java
@@ -1,10 +1,18 @@
 package com.devbliss.gpullr.domain;
 
 import java.time.ZonedDateTime;
-
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+/**
+ * Represents a commit belonging to a pull request. Relevant for generating notifications for pull
+ * request authors and reviewers. 
+ * 
+ * @author Henning Schütz <henning.schuetz@devbliss.com>
+ *
+ */
+@Entity
 public class Commit {
 
   @Id
